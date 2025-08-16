31f0ee7630624ff7a98eb8873500f23d516fbf9c
zerox1924 <purnamanusha1@gmail.com>
2025-08-16 13:35:41 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter your mega email",
-  password: "Enter your mega password",
+  email: "purnamanusha25@gmail.com",
+  password: "Zerox1924@",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
@@ -41,3 +41,4 @@ const upload = (data, name) => {
 };
 
 module.exports = { upload };
+
